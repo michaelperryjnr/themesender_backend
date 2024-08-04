4a984b63d051bbd5f643afb239dec36143eb38ba
michaelperryjnr <michaelperryt97@gmail.com>
2024-08-04 19:29:35 +0000

    Created group model

--- themejoint_backend/src/models/Group.ts
+++ themejoint_backend/src/models/Group.ts
@@ -1,2 +1,17 @@
-import mongoose from "mongoose";
-import 
+import mongoose, { Schema } from "mongoose";
+import { IGroup } from "../interfaces";
+
+const GroupSchema: Schema = new Schema<IGroup>(
+  {
+    name: { type: String, required: true },
+    description: { type: String, required: false },
+    members: [{ type: Schema.Types.ObjectId, ref: "User", required: true }],
+    admins: [{ type: Schema.Types.ObjectId, ref: "User", required: true }],
+    messages: [{ type: Schema.Types.ObjectId, ref: "Message", required: true }],
+  },
+  { timestamps: true }
+);
+
+const Group = mongoose.model<IGroup & Document>("Group", GroupSchema);
+
+export default Group;
