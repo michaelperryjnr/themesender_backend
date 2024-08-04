ee01b584ad72f99eba300ba4e984c48fd8a2202c
michaelperryjnr <michaelperryt97@gmail.com>
2024-08-04 19:30:36 +0000

    default export

--- themejoint_backend/src/models/index.ts
+++ themejoint_backend/src/models/index.ts
@@ -1,3 +1,8 @@
 import User from "./User";
+import Group from "./Group";
+import Token from "./Token";
+import Message from "./Message";
+import Call from "./Call";
+import Status from "./Status";
 
-export { User };
+export { User, Group, Token, Message, Call, Status };
